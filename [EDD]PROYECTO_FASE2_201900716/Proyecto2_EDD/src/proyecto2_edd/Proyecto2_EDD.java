d59c273f93080f995bf3a11240fabde89ddb5aae
Kelly901 <72952374+Kelly901@users.noreply.github.com>
2022-03-13 15:58:41 -0600

    commit 1

---
+++ [EDD]PROYECTO_FASE2_201900716/Proyecto2_EDD/src/proyecto2_edd/Proyecto2_EDD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2_edd;
+
+import Interfaz.Principal;
+
+/**
+ *
+ * @author Kelly
+ */
+public class Proyecto2_EDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo1");
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
